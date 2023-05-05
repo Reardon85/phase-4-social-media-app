7ac39f78a074e51fdf693f19eeb77881eb76f967
Nick Gastis <nickgastis1@gmail.com>
2023-05-05 13:14:57 -0400

    wireframe

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
