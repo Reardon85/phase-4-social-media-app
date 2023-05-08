21718163d9300070a2844156b3e4c79122c0408c
Brian Reardon <brian.d.reardon@gmail.com>
2023-05-08 19:23:46 -0400

    added password hash and authentication to models and the related flask routes, also added __table_name__ to the tables so they are plural

---
+++ server/config.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+
+
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+    )
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+
+
+migrate = Migrate(app, db)
+api = Api(app)
+
+bcrypt = Bcrypt(app)
+
+db.init_app(app)
