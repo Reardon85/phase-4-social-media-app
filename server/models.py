ca520a8c7b7785ba83d06b055da78e7a8718d9ff
Nick Gastis <nickgastis1@gmail.com>
2023-05-08 21:02:18 -0400

    posts

--- server/models.py
+++ server/models.py
@@ -8,7 +8,10 @@ db = SQLAlchemy()
 
 
 
-
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
 
 
 
