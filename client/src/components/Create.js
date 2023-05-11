3c15aebcfc85aa8bc0e16df1481cdb84fffc725f
Nick Gastis <nickgastis1@gmail.com>
2023-05-11 17:39:50 -0400

    foryouupdate

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,6 +1,6 @@
 
 
-import React, {useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import "./styles/Create.css"
 
 function Create() {
@@ -36,16 +36,16 @@ function Create() {
 
   return (
     <div class="upload-container">
-    <h1>Upload an Image</h1>
-    <input type="file" onChange={handleFileChange} accept="image/*" />
+      <h1>Upload an Image</h1>
+      <input type="file" onChange={handleFileChange} accept="image/*" />
       {file && (
         <div>
           <img src={URL.createObjectURL(file)} alt="Selected file" />
         </div>
       )}
-    <input type="text" placeholder="Add a caption" /> 
-    <button onClick={handleSubmit}>Upload</button>
-</div>
+      <input type="text" placeholder="Add a caption" />
+      <button onClick={handleSubmit}>Upload</button>
+    </div>
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,46 +1,46 @@
 import React, { useEffect, useState } from "react"
 import "./styles/Home.css"
 import Post from "./Post"
-function Home({setRefreshState, refreshState}) {
+function Home({ setRefreshState, refreshState }) {
 
-    const [posts, setPosts] = useState([])
-    const [total, setTotal] = useState(5)
-    const [morePosts, setMorePosts] = useState(true)
+  const [posts, setPosts] = useState([])
+  const [total, setTotal] = useState(5)
+  const [morePosts, setMorePosts] = useState(true)
 
-    useEffect(() => {
+  useEffect(() => {
 
-        fetch(`/home/${total}`)
-            .then((r) => {
-                if (r.ok) {
-                    r.json().then((data) => {
+    fetch(`/home/${total}`)
+      .then((r) => {
+        if (r.ok) {
+          r.json().then((data) => {
 
-                        setPosts(data.posts)
-                        setMorePosts(data.more_posts)
-                    })
-                }
-            })
-    }, [total])
-
-
+            setPosts(data.posts)
+            setMorePosts(data.more_posts)
+          })
+        }
+      })
+  }, [total])
 
 
-    const post_array =  posts.map((post) => (
-        <div className="post-container" key={post.id}>
-          <Post {...post} />
-        </div>
-      ))
-    
 
-    return (
-        <div className="home-container">
-          <div className="card">
-            {post_array}
-          </div>
-          <button className="more-btn" onClick={() => setTotal((total) => total + 5)}>
-            {morePosts ?  "MORE POSTS" : "NO MORE POSTS" }
-          </button>
-        </div>
-      )
-    }
+
+  const post_array = posts.map((post) => (
+    <div className="post-container" key={post.id}>
+      <Post {...post} />
+    </div>
+  ))
+
+
+  return (
+    <div className="home-container">
+      <div className="card">
+        {post_array}
+      </div>
+      <button className="more-btn" onClick={() => setTotal((total) => total + 5)}>
+        {morePosts ? "MORE POSTS" : "NO MORE POSTS"}
+      </button>
+    </div>
+  )
+}
 
 export default Home
