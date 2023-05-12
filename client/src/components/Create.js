9fdc5be281b7aa3b6c63ab7e5fb10c397aed38d7
Brian Reardon <brian.d.reardon@gmail.com>
2023-05-12 09:13:45 -0400

    updated route

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,17 +1,21 @@
-
-
 import React, { useState, useRef } from 'react';
 import "./styles/Create.css"
 
 function Create() {
   const [file, setFile] = useState(null);
+  const [caption, setCaption] = useState(null)
 
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
     setFile(selectedFile);
   };
 
-  const handleSubmit = async (event) => {
+  const handleCaptionChange = (event) => {
+    console.log(event.target.value)
+    setCaption((caption)=> event.target.value)
+  }
+  
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     if (!file) {
@@ -21,12 +25,19 @@ function Create() {
 
     const formData = new FormData();
     formData.append('image', file);
+    formData.append('content', caption)
+    console.log(formData)
 
     try {
-      await fetch('/upload_image', {
+       fetch('/create-post', {
         method: 'POST',
         body: formData,
-      });
+      })
+      .then((r) => r.json())
+      .then((d) => {
+        setFile(null)
+        setCaption(null);
+      })
     } catch (error) {
       console.error('Error occurred during image upload:', error);
     }
@@ -43,7 +54,7 @@ function Create() {
           <img src={URL.createObjectURL(file)} alt="Selected file" />
         </div>
       )}
-      <input type="text" placeholder="Add a caption" />
+      <input type="text" onChange={handleCaptionChange} placeholder="Add a caption"  value={caption}/>
       <button onClick={handleSubmit}>Upload</button>
     </div>
   );
