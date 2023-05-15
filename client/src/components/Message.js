c4cf012a33dbc6b868db27a0070b05cef9725330
Brian Reardon <brian.d.reardon@gmail.com>
2023-05-15 09:32:10 -0400

    added scroll bar

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './styles/Comment.css';
+import { Link } from 'react-router-dom';
 
-const Message = ({ username, content, timestamp, avatar }) => {
+const Message = ({ username, content, timestamp, avatar, userId }) => {
   return (
     <div className="comment">
       <div className="comment-header">
+        <Link to={`/profile/${userId}`}>
         <img className="profile-pic" src={avatar} alt="Author's profile" />
+        </Link>
         <h3 className="comment-author">{username}</h3>
       </div>
       <p className="comment-text">{content}</p>
